7809d20bc7fb6d853798869f3f385ccbe287ff93
tuk26019 <tuk26019@temple.edu>
2024-03-28 11:41:32 -0400

    create and used viewPager

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -5,12 +5,20 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        viewPager.adapter = object: FragmentStateAdapter(this){
+            override fun getItemCount() = 10
+
+            override fun createFragment(p0: Int) = TextFragment.newInstance((p0 +1).toString())
+        }
 
     }
 
